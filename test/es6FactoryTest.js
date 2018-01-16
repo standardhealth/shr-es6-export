7bb56583298d333059552dad80a6ec5a7dcb18ff
Chris Moesel <cmoesel@mitre.org>
2018-01-16 15:52:37 -0500

    Support instance creation w/out specifying type
    The `createInstance` functions now take the JSON as the _first_ argument, and the type as an optional second argument.  The code will try to determine the type from the JSON, but if it can't, it will fall back on the type passed in (if relevant).
    
    Also bump version to 5.2.0-alpha.2.

--- test/es6FactoryTest.js
+++ test/es6FactoryTest.js
@@ -11,32 +11,40 @@ describe('#ObjectFactory()', () => {
   const StringValueEntry = importResult('shr/simple/StringValueEntry');
 
   it('should create classes by name', () => {
-    const pv = ObjectFactory.createInstance('http://standardhealthrecord.org/spec/shr/simple/StringValueEntry');
+    const pv = ObjectFactory.createInstance({}, 'http://standardhealthrecord.org/spec/shr/simple/StringValueEntry');
     expect(pv).instanceOf(StringValueEntry);
     expect(pv.entryInfo).to.be.undefined;
     expect(pv.value).to.be.undefined;
     expect(pv.string).to.be.undefined;
   });
 
+  it('should throw when you request an element in the wrong namespace', () => {
+    expect(() => ObjectFactory.createInstance({}, 'http://standardhealthrecord.org/spec/shr/base/StringValueEntry')).to.throw();
+  });
+
   it('should throw when you request an unknown element', () => {
-    expect(() => ObjectFactory.createInstance('http://therealworld.org/Unicorn')).to.throw();
+    expect(() => ObjectFactory.createInstance({}, 'http://therealworld.org/Unicorn')).to.throw();
   });
 });
 
 describe('#NamespaceObjectFactory()', () => {
-  const ShrTestObjectFactory = importResult('shr/simple/ShrSimpleObjectFactory');
+  const ShrSimpleTestObjectFactory = importResult('shr/simple/ShrSimpleObjectFactory');
   const StringValueEntry = importResult('shr/simple/StringValueEntry');
 
   it('should create classes by name', () => {
-    const pv = ShrTestObjectFactory.createInstance('StringValueEntry');
+    const pv = ShrSimpleTestObjectFactory.createInstance({}, 'http://standardhealthrecord.org/spec/shr/simple/StringValueEntry');
     expect(pv).instanceOf(StringValueEntry);
     expect(pv.entryInfo).to.be.undefined;
     expect(pv.value).to.be.undefined;
     expect(pv.string).to.be.undefined;
   });
 
+  it('should throw when you request an element from a different namespace', () => {
+    expect(() => ShrSimpleTestObjectFactory.createInstance({}, 'http://standardhealthrecord.org/spec/shr/base/StringValueEntry')).to.throw();
+  });
+
   it('should throw when you request an unknown element', () => {
-    expect(() => ShrTestObjectFactory.createInstance('Unicorn')).to.throw();
+    expect(() => ShrSimpleTestObjectFactory.createInstance({}, 'http://standardhealthrecord.org/spec/shr/simple/Unicorn')).to.throw();
   });
 });
 
